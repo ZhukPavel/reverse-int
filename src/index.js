e2cd71a8dc0f5fb70f85a348c6c956cc20dc2932
ZhukPavel <pabloescarabajo@live.com>
2022-01-04 10:23:57 +0300

    feat:update function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+ let str=String(Math.abs(n));
+  let arr=[];
+  for (i=0; i<=str.length-1;i++){
+      arr[i]=str[i];
+  }
+  arr = arr.reverse();
+  result=arr.join('');
+  return result;
 }
